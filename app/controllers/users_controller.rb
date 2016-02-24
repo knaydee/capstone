aea35ef65882a7bcaeff495bca27f423ebae90d8
Meighan Rasley <meighanr@gmail.com>
2016-02-24 15:12:59 -0800

    .env file is loading correctly and Google map is now showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    @key = ENV["GOOGLE_API_KEY"]
+    @map = HTTParty.get("https://www.google.com/maps/embed/v1/search?key=#{@key}")
   end
-  
+
 end
