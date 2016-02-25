d09d011d881d6cfd0eebceda113dc532aa876b51
Meighan Rasley <meighanr@gmail.com>
2016-02-25 10:56:27 -0800

    Able to pass in zip as parameter and search field now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     @key = ENV["GOOGLE_API_KEY"]
-    @map = HTTParty.get("https://www.google.com/maps/embed/v1/search?key=#{@key}")
+    @zip = params[:zipsearch]
   end
 
 end
