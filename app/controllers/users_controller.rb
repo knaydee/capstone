fc3110413676b90c2188105c2766867ff25a0c0c
Meighan Rasley <meighanr@gmail.com>
2016-02-29 14:25:08 -0800

    nothing works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
+require 'pry'
 class UsersController < ApplicationController
 
   def index
     @key = ENV["GOOGLE_API_KEY"]
     @term = params[:termsearch]
-    @map = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=veterinar+near+<%= @term %>&key=<%= @key %>"
+    @map = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=veterinar+near+#{@term}&key=#{@key}"
     @data = HTTParty.get(@map)
   end
 
