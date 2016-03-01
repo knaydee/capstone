efed181a3822ce7fc1a5500bc59522de9a442736
Meighan Rasley <meighanr@gmail.com>
2016-03-01 09:50:30 -0800

    Google OAuth works

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'https://localhost:3000'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     :name => "google",
