f526ffbddacd4639ab56aae358efe23b6cb6a19d
Meighan Rasley <meighanr@gmail.com>
2016-02-29 17:31:47 -0800

    now know what @data is showing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
 
   def index
     @key = ENV["GOOGLE_API_KEY"]
-    @zip = params[:zipsearch]
+    @zip = params[:termsearch]
+    @map = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=veterinar+near+98103&key=#{@key}"
+    @data = HTTParty.get(@map)
   end
 
 end
