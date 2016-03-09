feca00367f6a83df3a4b44b47467bb778959e9ad
Meighan Rasley <meighanr@gmail.com>
2016-03-09 11:56:45 -0800

    services selection field will open appropriate view based on selection

--- app/assets/javascripts/petstop.js
+++ app/assets/javascripts/petstop.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
 
   $('#vet_services').change(function() {
     var vet_id = $('form').attr('id').slice(-1);
-    console.log(vet_id);
+    var form_id = $('form').attr('id');
     $('form').attr('url', '/vets/'+vet_id+'/services/'+$(this).val());
+    window.location = $('form').attr('url');
   });
+
 });
