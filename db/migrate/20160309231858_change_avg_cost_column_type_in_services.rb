ffa0677a415a3e7c3a166bf4609e217a5f5deda9
Meighan Rasley <meighanr@gmail.com>
2016-03-09 15:20:10 -0800

    more migration fixes

--- db/migrate/20160309231858_change_avg_cost_column_type_in_services.rb
+++ db/migrate/20160309231858_change_avg_cost_column_type_in_services.rb
@@ -1,4 +1,4 @@
-class ChangeAvgCostColumnTypeInService < ActiveRecord::Migration
+class ChangeAvgCostColumnTypeInServices < ActiveRecord::Migration
   def change
     change_column :services, :avg_cost, :integer
   end
