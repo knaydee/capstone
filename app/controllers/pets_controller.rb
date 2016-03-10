c58d11685a9cd161b83b9ccc79d820f31280a881
Meighan Rasley <meighanr@gmail.com>
2016-03-10 10:17:48 -0800

    Form to add a new pet, edit a pet, edit and create methods

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,4 +15,28 @@ class PetsController < ApplicationController
     @action = "create"
   end
 
+  def create
+    @pet = Pet.create(pet_params[:pet])
+    redirect_to user_pets_path(@current_user)
+  end
+
+  def edit
+    id = params[:id]
+    @pet = Pet.find(id)
+    @action = "update"
+  end
+
+  private
+
+  def pet_params
+    params.permit(pet:[:name, :age, :breed, :weight, :size, :sex, :species])
+  end
+
+  def only_owner
+      if !@current_user || @current_user.id != Pet.find(params[:id]).user_id
+        flash[:error] = "You are not authorized to view that section"
+        redirect_to root_path
+      end
+  end
+
 end
