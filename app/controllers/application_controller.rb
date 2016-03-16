0f68a17a2215a384342d13c163c11980963675e9
Meighan Rasley <meighanr@gmail.com>
2016-03-16 11:39:57 -0700

    helper method to link vet addresses to google maps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     if current_user.nil?
-      flash[:error] = "You must be logged in to view this section"
-      redirect_to root_path
+      redirect_to '/auth/google'
     end
   end
 end
--- app/helpers/vets_helper.rb
+++ app/helpers/vets_helper.rb
@@ -1,2 +1,6 @@
 module VetsHelper
+  def address_link(address)
+    url = "https://www.google.com/maps/search/#{address}/"
+    link_to address, url
+  end
 end
