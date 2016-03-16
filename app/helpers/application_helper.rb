8f58083657df07623be2ad10c16a37b56303c2f3
Meighan Rasley <meighanr@gmail.com>
2016-03-16 16:58:33 -0700

    styling welcome page and nav bar for both signed in guests and visitors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,20 @@ require "action_view/helpers/number_helper"
 module ApplicationHelper
   include ActionView::Helpers::NumberHelper
   def formatted_price(price)
-    price = price / 100.0
+    price = price.to_i / 100.0
     number_to_currency(price)
+  end
+
+  def colorize_price(price, avg_cost)
+    color = if avg_cost.to_i == 0
+      "black"
+    elsif price.to_i > avg_cost.to_i
+      "red"
+    elsif price.to_i < avg_cost.to_i
+      "green"
+    else
+      "black"
+    end
+    return "<font color=#{color}>#{formatted_price(price)}</font>".html_safe
   end
 end
