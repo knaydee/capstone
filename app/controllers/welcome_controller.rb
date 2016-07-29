976639315dd725dd078c45e4e439ed6114b6ca2b
Meighan Rasley <meighanr@gmail.com>
2016-07-28 19:15:16 -0700

    Remove the puts'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    puts "Hii"
-    puts params
     @params_search = params[:search]
     if params[:search]
       @vets = Vet.search(params[:search]).order("name ASC")
-      puts "Helllo", Vet.search(params[:search])
-      puts params[:search]
-      puts Vet.search(params[:search])
     else
       @vets = Vet.all.order('name ASC')
     end
